f5b1557623f7dc9edb569ee2accba135b2c495cb
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2024-04-22 15:52:29 -0300

    styles navbar using styled components

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,9 +1,20 @@
+import styled from 'styled-components';
+
 export const Nav = () => {
     return (
         <nav>
-            <a href="#about-section">About</a>
-            <a href="#skills-section">Skills</a>
-            <a href="#projects-section">Projects</a>
+            <Anchor href="#about-section">About</Anchor>
+            <Anchor href="#skills-section">Skills</Anchor>
+            <Anchor href="#projects-section">Projects</Anchor>
         </nav>
     );
 };
+
+const Anchor = styled.a`
+    margin-right: 1em;
+    color: inherit;
+    text-decoration: none;
+    padding: 10px;
+    background-color: rgb(21, 18, 242);
+    border-radius: 10%;
+`;
